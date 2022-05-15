d8e26fe6e0e876abfb858cbc7a257fd8cebfbfd2
Christopher Luna <dev@christopher-luna.com>
2022-05-15 02:20:21 -0500

    Use $PROXY with iptables

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@ fi
 sed -e "s/<IP_SERVER>/${IP_SERVER}/" -e "s/<IP_CLIENT>/${IP_CLIENT}/" /ppp_options > /etc/ppp/options
 /usr/bin/tcpser -i"&c0" -l4 -s ${BAUD} -d ${DEV} -n ${PPP_PHONE}=127.0.0.1:2323 $n &
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+if [ ! -z "$PROXY" ]; then
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $PROXY
+fi
 dnsmasq &
 
 rm -f /pppd-fifo
